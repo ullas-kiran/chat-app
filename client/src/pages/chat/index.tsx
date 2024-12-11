c5bac8b73e0d92ad3d79332f2f30e6caad623632
ullas-kiran <ullas261@gmail.com>
2024-12-11 16:43:28 +0530

    feat(react-lottie):added for animation

--- client/src/pages/chat/index.tsx
+++ client/src/pages/chat/index.tsx
@@ -2,18 +2,25 @@ import { useAppStore } from "@/store"
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ContactsContainer from "./components/contact-container";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ChatContainer from "./components/chat-container";
 
 const Chat:React.FunctionComponent = ():JSX.Element => {
   const {userInfo} =useAppStore();
   const navigate =useNavigate()
   useEffect(()=>{
-    if(userInfo?.profileSetup){
+    if(!userInfo?.profileSetup){
       toast("Please setup profile to continue");
       navigate("/profile")
     }
   },[userInfo,navigate])
     return (
-      <div>Chat</div>
+      <div className="flex h-[100vh] text-white overflow-hidden">
+        <ContactsContainer/>
+        <EmptyChatContainer/>
+        <ChatContainer/>
+      </div>
     )
   }
   
